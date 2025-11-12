fc5323f598a4b4b3311f3fc4705c6f3d8c9c95f5
sbx_user1051 <sbx_user1051@169.254.42.221>
2025-11-12 21:13:31 +0000

    Wed Nov 12 21:13:31 UTC 2025

---
+++ src/test/java/io/accelerate/solutions/DMO/InventoryTest.java
@@ -0,0 +1,11 @@
+package io.accelerate.solutions.DMO;
+
+import org.junit.jupiter.api.Test;
+
+public class InventoryTest {
+    @Test
+    public void storesInventory() {
+        var inventory = new DemoRound3Solution();
+        inventory.inventoryAdd(new InventoryItem())
+    }
+}
