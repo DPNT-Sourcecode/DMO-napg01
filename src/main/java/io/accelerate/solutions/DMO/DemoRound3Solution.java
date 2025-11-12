25394fa5a05ec0230e3e8b2230ee5bf17f0a0b0e
sbx_user1051 <sbx_user1051@169.254.42.221>
2025-11-12 21:19:31 +0000

    Wed Nov 12 21:19:31 UTC 2025

--- src/main/java/io/accelerate/solutions/DMO/DemoRound3Solution.java
+++ src/main/java/io/accelerate/solutions/DMO/DemoRound3Solution.java
@@ -1,19 +1,24 @@
 package io.accelerate.solutions.DMO;
 
 import io.accelerate.runner.SolutionNotImplementedException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class DemoRound3Solution {
+  private final ConcurrentMap<InventoryItem, Integer> stock = new ConcurrentHashMap<>();
 
-    public int inventoryAdd(InventoryItem item, int quantity) {
-        return quantity;
-    }
+  public int inventoryAdd(InventoryItem item, int quantity) {
+    stock.put(item, quantity);
+    return quantity;
+  }
 
-    public int inventorySize() {
-        throw new SolutionNotImplementedException();
-    }
+  public int inventorySize() {
+    return stock.size();
+  }
 
-    public InventoryItem inventoryGet(String sku) {
-        throw new SolutionNotImplementedException();
-    }
+  public InventoryItem inventoryGet(String sku) {
+    throw new SolutionNotImplementedException();
+  }
 }
+
 
