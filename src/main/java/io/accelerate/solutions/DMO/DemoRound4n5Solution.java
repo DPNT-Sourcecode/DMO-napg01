9dd6d19a42ea99b812bd5ecb32b113722860fc9f
sbx_user1051 <sbx_user1051@169.254.42.221>
2025-11-12 21:31:46 +0000

    Wed Nov 12 21:31:46 UTC 2025

--- src/main/java/io/accelerate/solutions/DMO/DemoRound4n5Solution.java
+++ src/main/java/io/accelerate/solutions/DMO/DemoRound4n5Solution.java
@@ -3,9 +3,10 @@ package io.accelerate.solutions.DMO;
 public class DemoRound4n5Solution {
   public String waves(Integer numberOfWaves) {
     var outputSink = new Waves.StringOutputSink();
-    Waves.run(outputSink, () -> numberOfWaves);
-    return outputSink.content();
+    Waves.run(outputSink, numberOfWaves);
+    return outputSink.content().stripTrailing();
   }
 }
+
 
 
--- src/test/java/io/accelerate/solutions/DMO/WavesTest.java
+++ src/test/java/io/accelerate/solutions/DMO/WavesTest.java
@@ -1,51 +1,31 @@
 package io.accelerate.solutions.DMO;
 
-import org.junit.jupiter.api.Test;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
-public class InventoryTest {
+import org.junit.jupiter.api.Test;
+
+public class WavesTest {
   @Test
-  public void storesItems() {
-    var inventory = new DemoRound3Solution();
-    var quantity = inventory.inventoryAdd(new InventoryItem("SKU", "name", 7), 3);
-    assertThat(quantity, equalTo(3));
-    assertThat(inventory.inventorySize(), equalTo(1));
+  public void oneWave() {
+    var waves = new DemoRound4n5Solution();
+    var result = waves.waves(1);
+    assertThat(result, is("____....~~~~''''~~~~....____"));
   }
 
   @Test
-  public void storesMultipleItems() {
-    var inventory = new DemoRound3Solution();
-    inventory.inventoryAdd(new InventoryItem("SKU", "name", 7), 3);
-    var quantity = inventory.inventoryAdd(new InventoryItem("ABC", "thing", 8), 4);
-    assertThat(quantity, equalTo(4));
-    assertThat(inventory.inventorySize(), equalTo(2));
+  public void twoWaves() {
+    var waves = new DemoRound4n5Solution();
+    var result = waves.waves(2);
+    assertThat(result, is("__..~~''~~..____..~~''~~..__"));
   }
 
-    @Test
-    public void remembersQuantity() {
-        var inventory = new DemoRound3Solution();
-        inventory.inventoryAdd(new InventoryItem("SKU", "name", 7), 3);
-        var quantity = inventory.inventoryAdd(new InventoryItem("SKU", "name", 7), 4);
-        assertThat(quantity, equalTo(7));
-        assertThat(inventory.inventorySize(), equalTo(1));
-    }
-
-    @Test
-    public void getsItemBySku() {
-        var inventory = new DemoRound3Solution();
-        InventoryItem storedItem = new InventoryItem("SKU", "name", 7);
-        inventory.inventoryAdd(storedItem, 3);
-        var retrievedItem = inventory.inventoryGet("SKU");
-        assertThat(retrievedItem, equalTo(storedItem));
-    }
-
-    @Test
-    public void returnsNullIfItemIsNotFoundBySku() {
-        var inventory = new DemoRound3Solution();
-        inventory.inventoryAdd(new InventoryItem("SKU", "name", 7), 3);
-        var retrievedItem = inventory.inventoryGet("wibble");
-        assertThat(retrievedItem, equalTo(null));
-    }
+  @Test
+  public void threeWaves() {
+    var waves = new DemoRound4n5Solution();
+    var result = waves.waves(3);
+    assertThat(result, is("_.~'~.__.~'~.__.~'~._"));
+  }
 }
+
