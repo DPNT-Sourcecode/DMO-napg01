e9de831abfe9ef26ff2c5cb96c6ed8ff63c50255
sbx_user1051 <sbx_user1051@169.254.42.221>
2025-11-12 21:01:39 +0000

    Wed Nov 12 21:01:39 UTC 2025

--- src/main/java/io/accelerate/solutions/SUM/SumSolution.java
+++ src/main/java/io/accelerate/solutions/SUM/SumSolution.java
@@ -1,12 +1,8 @@
 package io.accelerate.solutions.SUM;
 
-import io.accelerate.runner.SolutionNotImplementedException;
-
-@SuppressWarnings("unused")
 public class SumSolution {
-
-    public int compute(int x, int y) {
-        throw new SolutionNotImplementedException();
-    }
-
+  public int compute(int x, int y) {
+    return x + y;
+  }
 }
+
