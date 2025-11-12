fb70c2d9f5982d49b34038eb015c75cb708522ca
sbx_user1051 <sbx_user1051@169.254.42.221>
2025-11-12 21:10:30 +0000

    Wed Nov 12 21:10:30 UTC 2025

--- src/main/java/io/accelerate/solutions/DMO/DemoRound2Solution.java
+++ src/main/java/io/accelerate/solutions/DMO/DemoRound2Solution.java
@@ -3,17 +3,20 @@ package io.accelerate.solutions.DMO;
 import io.accelerate.runner.SolutionNotImplementedException;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class DemoRound2Solution {
     public int arraySum(List<Integer> integerList) {
-        throw new SolutionNotImplementedException();
+        return integerList.stream().mapToInt(x -> x).sum();
     }
 
     public List<Integer> intRange(int start, int end) {
-        throw new SolutionNotImplementedException();
+        return IntStream.range(start, end).boxed().toList();
     }
 
     public List<Integer> filterPass(List<Integer> integerList, int threshold) {
-        throw new SolutionNotImplementedException();
+        return integerList.stream().filter(x -> x >= threshold).toList();
     }
 }
+
