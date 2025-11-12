46a84c71aa170688286042c0063b79ca3d055ecb
sbx_user1051 <sbx_user1051@169.254.42.221>
2025-11-12 21:46:47 +0000

    Wed Nov 12 21:46:47 UTC 2025

--- src/main/java/io/accelerate/solutions/DMO/Waves.java
+++ src/main/java/io/accelerate/solutions/DMO/Waves.java
@@ -79,8 +79,6 @@ class Waves {
   }
 
   public static void run(OutputSink outputSink, float numberOfWaves) {
-    int label = 10;
-
     float scalarE = 7;
     float scalarI = 1;
     float scalarJ = 0;
@@ -95,57 +93,58 @@ class Waves {
 
     int iterations = 0;
 
-    mainLoop:
-    while (true) {
-      iterations += 1;
-      if (iterations > 99999) {
-        output.print("INFINITE LOOP DETECTED. STOPPING EXECUTION.");
-        output.println();
-        break;
-      }
+    i:
+    for (int i = 0; i < numberOfWaves; i++) {
+      int label = 10;
+      loopActive11 = false;
 
-      if (loopActive11 && label > 13) loopActive11 = false;
+      mainLoop:
+      while (true) {
+        iterations += 1;
+        if (iterations > 99999) {
+          output.print("INFINITE LOOP DETECTED. STOPPING EXECUTION.");
+          output.println();
+          break;
+        }
 
-      switch (label) {
-        // 10FORI=1TOFSTEP1
-        case 10:
-          label = 11;
-          if (scalarI - numberOfWaves > 0) {
-            break mainLoop;
-          }
-          break;
-        // 11FORJ=1TOLSTEPMS
-        case 11:
-          label = 12;
-          if (!loopActive11) {
-            scalarJ = 1;
-            loopActive11 = true;
-          }
-          if ((scalarJ - waveStringLength) * scalarMS > 0) {
-            label = 14;
-          }
-          break;
-        // 12PRINTMID$(W$,J,S);
-        case 12:
-          label = 13;
-          output.print(mid(waveString, scalarJ, charsPerIteration));
-          break;
-        // 13NEXTJ
-        case 13:
-          scalarJ = scalarJ + scalarMS;
-          label = 11;
-          break;
-        // 14NEXTI
-        case 14:
-          scalarI = scalarI + 1;
-          label = 10;
-          break;
-        default:
-          throw new IllegalStateException("The label " + label + " is not recognized.");
+        switch (label) {
+          // 10FORI=1TOFSTEP1
+          case 10:
+            label = 11;
+            break;
+          // 11FORJ=1TOLSTEPMS
+          case 11:
+            label = 12;
+            if (!loopActive11) {
+              scalarJ = 1;
+              loopActive11 = true;
+            }
+            if ((scalarJ - waveStringLength) * scalarMS > 0) {
+              label = 14;
+            }
+            break;
+          // 12PRINTMID$(W$,J,S);
+          case 12:
+            label = 13;
+            output.print(mid(waveString, scalarJ, charsPerIteration));
+            break;
+          // 13NEXTJ
+          case 13:
+            scalarJ = scalarJ + scalarMS;
+            label = 11;
+            break;
+          // 14NEXTI
+          case 14:
+            scalarI = scalarI + 1;
+            continue i;
+          default:
+            throw new IllegalStateException("The label " + label + " is not recognized.");
+        }
       }
     }
     output.println();
   }
 }
+
 
 
